e9620a6f2da4614e1558eef199a52e72b0145417
Yusuf Damiloju <yusufdamiloju@gmail.com>
2017-03-07 16:02:50 +0100

    WIP

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
     <div class="container">
+        <div class="jumbotron">
+            <h2>Welcome To Lucid Birthday Search Application</h2>
+            <p>Click button below to view birthdays</p>
+            <p><a class="btn btn-primary btn-lg" role="button" href="{{route('view')}}">Birthdays</a></p>
+        </div>
+
         @foreach($birthdays as $birthday)
             <div class="alert alert-danger alert-dismissible" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -9,10 +15,6 @@
                 <strong>Birthday!!!</strong> {{$birthday->name}}'s birthday is today
             </div>
         @endforeach
-        <div class="jumbotron">
-            <h2>Welcome To Lucid Birthday Search Application</h2>
-            <p>Click button below to view birthdays</p>
-            <p><a class="btn btn-primary btn-lg" role="button" href="{{route('view')}}">Birthdays</a></p>
-        </div>
+
     </div>
 @stop
