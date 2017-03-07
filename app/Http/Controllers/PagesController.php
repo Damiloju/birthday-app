dddc0cdf0abfb0686e9be714d4c724db2f1fc343
Yusuf Damiloju <yusufdamiloju@gmail.com>
2017-03-07 15:56:05 +0100

    Added Birthday alert feature

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,13 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Records;
 class PagesController extends Controller
 {
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function home(){
+
+        $month =  date('F');
+        $day = date('j');
+
+        $birthdays = Records::where('month',$month)->where('day', $day)->get();
+
+        if(!Empty($birthdays)){
+            return view('pages.home' ,compact('birthdays'));
+        }
+
         return view('pages.home');
     }
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div class="container">
+        @foreach($birthdays as $birthday)
+            <div class="alert alert-danger alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+                <strong>Birthday!!!</strong> {{$birthday->name}}'s birthday is today
+            </div>
+        @endforeach
         <div class="jumbotron">
             <h2>Welcome To Lucid Birthday Search Application</h2>
             <p>Click button below to view birthdays</p>
